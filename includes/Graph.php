f8fcef26d44a609261d4088f2d7e1bf579d0ab6a
Gavin Hanson <gavin@invigle.com>
2013-09-11 19:38:50 +0100

    Added editProperties function to Graph.php

--- includes/Graph.php
+++ includes/Graph.php
@@ -72,7 +72,7 @@ class Graph extends Search {
         
         $index->add($node, $params['indexBy'], $node->getProperty($params['indexBy']));
         $rtn = $index->queryOne("$params[indexBy]:$params[username]");
-        print '<pre>';
+        print 'RETURN:<hr><pre>';
         print_r($rtn['id']);
         print '</pre>';
 	}
