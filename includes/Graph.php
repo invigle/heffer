9b9eb511966dd39546930703bfdc79e31b290cff
Gavin Hanson <gavin@invigle.com>
2013-09-11 22:57:54 +0100

    Added add connection to Graph.php

--- includes/Graph.php
+++ includes/Graph.php
@@ -110,7 +110,7 @@ class Graph extends Search {
 	 * @param $aID
 	 */
 	public function deleteNodeByID($aID) {
-        $node->$this->_client->getNode($aID);        
+        $node = $this->_client->getNode($aID);        
         $node->delete();
 	}
 
@@ -121,7 +121,7 @@ class Graph extends Search {
 	 * @param aType
 	 */
 	public function addConnection($aID1, $aID2, $aType) {
-        $node1->$this->_client->getNode(int($aID1));
+        $node1 = $this->_client->getNode($aID1);
         //$node2->$this->_client->getNode($aID2);
         //$node1->relateTo($node2, "$aType")->save();
         
