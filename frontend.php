e8ec94b0104a2167be9b6fac107f3c40a86fef25
Rid <grant@seednet.eu>
2013-09-11 22:38:53 +0100

    added front end test

--- frontend.php
+++ frontend.php
@@ -7,14 +7,11 @@
 
 require_once("includes/xhp/php-lib/init.php");
 
-		//$html = "<!DOCTYPE HTML>";
-        $post =
-  <div class="post">
-    <h2>{$post}</h2>
-    <p><span>Hey there.</span></p>
-    <a href={$like_link}>Like</a>
-  </div>;
-                
-        echo $post;
+        $html = "<!DOCTYPE HTML>
+                    <html>";
+        $html += <head>
+                    <title>{$pageTitle}</title>
+                </head>;
+        echo $html;
 
 ?>
