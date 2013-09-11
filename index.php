75cdb58ea2482f002d1398b84c4364b2cad437f4
Rid <grant@seednet.eu>
2013-09-11 13:02:41 +0100

    added print

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-require("phar://includes/neo4jphp.phar");
+require("phar:///invigle/heffer/includes/neo4jphp.phar");
 
 // Connecting to a different port or host
 $client = new Everyman\Neo4j\Client('localhost', 8001);
 
+print_r($client->getServerInfo());
 ?>
