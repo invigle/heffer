1d6e4a078125c7ecb6968acd58b456990f1b1a76
Gavin Hanson <gavin@invigle.com>
2013-09-12 01:41:18 +0100

    Testing Neo4j RESTful API

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
 
-function apiCall($input) {
-	$url = "http://boss.invigle.com:8001/db/data/$input[path]";
+function apiCall($api, $type, $postfields = array()) {
+	$url = "http://boss.invigle.com:8001/db/data/$api[path]";
 
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
-	//curl_setopt($ch, CURLOPT_POST, 1);
 	curl_setopt($ch, CURLOPT_TIMEOUT, 100);
+        if($type === "POST"){
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);    
+        }
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	//curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
 	$data = curl_exec($ch);
 	curl_close($ch);
 
@@ -19,7 +21,8 @@ function apiCall($input) {
 
 $api['path'] = "node/6";
 
-$arr = apiCall($api);
+
+$arr = apiCall($api, 'GET');
 print 'WORK:<hr><pre>';
 print_r($arr);
 print '</pre>';
