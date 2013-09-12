a237cb59be7e78d1ade50988a7a5f7fa3b34f8b1
Gavin Hanson <gavin@invigle.com>
2013-09-12 16:33:18 +0100

    Added ListNodes to Graph.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ namespace Invigle;
 require_once("bootstrap.php");
 
 $graph = new Graph();
-$arr = $graph->listNodes('19', 'LINKED_TO', '0', '10');
+$arr = $graph->listNodes('19', 'FRIEND_OF', '0', '10');
 
 print 'WORK:<hr><pre>';
 print_r($arr);
