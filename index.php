e8e9ca354b5ea9b20b4657663e90708dcb33b926
Gavin Hanson <gavin@invigle.com>
2013-09-12 12:39:46 +0100

    Testing editProperties Function of Graph.php

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 namespace Invigle;
 require_once("bootstrap.php");
 
+$foo['indexBy'] = "username";
+$foo['indexValue'] = "dilbert101";
+
 $graph = new Graph();
-$arr = $graph->editProperties($foo = array());
+$arr = $graph->editProperties($foo);
 
 print 'WORK:<hr><pre>';
 print_r($arr);
