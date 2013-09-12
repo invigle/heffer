10465067662d10058e4ebf66b5d55f0a65c56020
Rid <grant@seednet.eu>
2013-09-12 18:38:55 +0100

    added topNav

--- lib/Invigle/FrontEndUIs.php
+++ lib/Invigle/FrontEndUIs.php
@@ -11,45 +11,66 @@ namespace Invigle;
  * @version 2013
  * @access public
  */
- 
+
 abstract class FrontEndUIs
 {
 
     protected $_pageTitle;
-    
+
     /**
      * This function renders the header
-	 * @access public
-	 */
-	public function renderHeader() {
-		return '<!DOCTYPE HTML>
+     * @access public
+     */
+    public function renderHeader()
+    {
+        return '<!DOCTYPE HTML>
                     <html>
                     <head>
                         <meta charset="utf-8">
                         <meta name="viewport" content="width=device-width, initial-scale=1.0">
                         <meta name="description" content="">
                         <meta name="author" content="">
-                        <title>'.$this->_pageTitle.'</title>
+                        <title>' . $this->_pageTitle . '</title>
                         <!-- Bootstrap core CSS -->
                         <link href="/assets/bootstrap/css/bootstrap.css" rel="stylesheet">
                     </head>';
-	}
+    }
 
     /**
      * This function renders the top nav bar
-	 * @access public
-	 */
-	public function renderTopNav() {
-		
-	}
-    
+     * @access public
+     */
+    public function renderTopNav()
+    {
+        return '<div class="navbar navbar-inverse navbar-fixed-top">
+                    <div class="container">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="#">Project name</a>
+                        </div>
+                        <div class="collapse navbar-collapse">
+                            <ul class="nav navbar-nav">
+                                <li class="active"><a href="#">Home</a></li>
+                                <li><a href="#about">About</a></li>
+                                <li><a href="#contact">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>';
+    }
+
     /**
      * This function renders the top nav bar
-	 * @access public
-	 */
-	public function renderSideBar() {
-		// Not yet implemented
-	}
+     * @access public
+     */
+    public function renderSideBar()
+    {
+        // Not yet implemented
+    }
 
     /**
      * Set page title
