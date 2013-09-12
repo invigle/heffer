3d2e9f9da6005e7dd4d726c08dc4b293cb1c4996
Gavin Hanson <gavin@invigle.com>
2013-09-12 22:56:32 +0100

    Testing User.php Changes

--- index.php
+++ index.php
@@ -11,10 +11,10 @@ $usr = array(
             'username'=>'jjameson',
             'sexualpref'=>'straight',
             'birthday'=>'1984-11-26',
-            'institution'=>'',
+            'institution'=>NULL,
             'relationshipstatus'=>'single',
             'gender'=>'female',
-            'profilepicid'=>'',
+            'profilepicid'=>NULL,
             'followercount'=>'0',
             'friendcount'=>'0'
             );
