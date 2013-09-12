cdd4d8c744a596c75182337e35b93982b387a572
Gavin Hanson <gavin@invigle.com>
2013-09-12 02:06:26 +0100

    Testing Neo4j RESTful API

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ namespace Invigle;
 require_once("bootstrap.php");
 
 $graph = new Graph();
-$arr = $graph->neo4japi("/node/6");
+$arr = $graph->neo4japi("node/6");
 
 print 'WORK:<hr><pre>';
 print_r($arr);
