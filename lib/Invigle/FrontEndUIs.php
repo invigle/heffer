439ee340516def6952fd8d116566daeb6ac0cecb
Rid <grant@seednet.eu>
2013-09-12 15:24:39 +0100

    testing frontend header

--- lib/Invigle/FrontEndUIs.php
+++ lib/Invigle/FrontEndUIs.php
@@ -22,7 +22,7 @@ abstract class FrontEndUIs
                     <html>";
         $html .= "<head>
                     <title>{$this->_pageTitle}</title>
-                </head>test";
+                </head>";
         return $html;
 	}
 
