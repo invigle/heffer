76576ed6931989552aab603d59730afb91deccf2
Gavin Hanson <gavin@invigle.com>
2013-09-13 22:40:52 +0100

    test commit2

--- lib/Invigle/Language/EN_GB.php
+++ lib/Invigle/Language/EN_GB.php
@@ -20,7 +20,7 @@ class EN_GB extends Language
     {
         $this->_frontPage = array(
             'pageTitle' => "Invigle - \"Let's Link Up!\"",
-            'register' => "Register",
+            'register' => "RegisterABC",
             'firstname' => "First Name",
             'lastname' => "Last Name",
             'emailaddress' => "Email Address",
