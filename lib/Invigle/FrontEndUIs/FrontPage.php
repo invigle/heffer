811ed410691151b632ebea7065a59b41d100fdb4
Gavin Hanson <gavin@invigle.com>
2013-09-13 14:53:21 +0100

    Starting register Form

--- lib/Invigle/FrontEndUIs/FrontPage.php
+++ lib/Invigle/FrontEndUIs/FrontPage.php
@@ -165,11 +165,15 @@ class FrontPage extends FrontEndUIs {
                     <input type="hidden" name="regform" value="submit">
                         <h2>'.$this->_language->_frontPage["register"].'</h2>
                         <div class="row-fluid">
-                            <div class="col-md-6">
-                                <input type="text" class="form-control col-md-6" name="firstname" value="'.$userArray['firstname'].'" placeholder="'.$this->_language->_frontPage["firstname"].'">
-                            </div>
-                            <div class="col-md-6">
-                                <input type="text" class="form-control col-md-6" name="lastname" value="'.$userArray['lastname'].'" placeholder="'.$this->_language->_frontPage["lastname"].'">
+                            <div class="col-md-12">
+                            
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control col-md-6" name="firstname" value="'.$userArray['firstname'].'" placeholder="'.$this->_language->_frontPage["firstname"].'">
+                                </div>
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control col-md-6" name="lastname" value="'.$userArray['lastname'].'" placeholder="'.$this->_language->_frontPage["lastname"].'">
+                                </div>
+                            
                             </div>
                         </div>
                         <div class="row-fluid">
