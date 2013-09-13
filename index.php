854aa50b7db06fcfa5bc457a27d8d323eacbff2a
Rid <grant@seednet.eu>
2013-09-13 23:36:16 +0100

    testing branches

--- index.php
+++ index.php
@@ -1,36 +1,5 @@
 <?php
-namespace Invigle;
-require_once("configuration.php");
-require_once("bootstrap.php");
 
 
-$usr = array(
-            'firstname'=>"Jenna",
-            'lasname'=>"Jameson",
-            'email'=>"hugetits@hotmail.com",
-            'password'=>'LovesC0CK',
-            'username'=>'jjameson',
-            'sexualpref'=>'straight',
-            'birthday'=>'1984-11-26',
-            'institution'=>'',
-            'relationshipstatus'=>'single',
-            'gender'=>'female',
-            'profilepicid'=>'',
-            'followercount'=>'0',
-            'friendcount'=>'0'
-            );
-
-$user = new User();
-$test = $user->validateUsernameFormatting($_GET['username']);
-
-if($test){
-    print 'Accepted';
-}else{
-    print 'Declined';
-}
-
-//$arr = $user->addUser($usr);
-
-//print $arr;
-
+echo "test";
 ?>
