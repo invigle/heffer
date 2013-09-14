1b0a354dddae7b707865c094b327e915cbf1d020
Grant Millar <grant@seednet.eu>
2013-09-15 00:46:15 +0100

    Update User.php

--- lib/Invigle/User.php
+++ lib/Invigle/User.php
@@ -38,95 +38,7 @@ class User
 	private $_profilePicID;
 	private $_followerCount;
 	private $_friendCount;
-<<<<<<< HEAD
-	private $_url;
-
-	/**
-	 * This method will check the graph database to ensure a username is unique.
-	 * @param username
-	 * @return boolean (true if Available, false if Taken)
-	 */
-	public function validateUsername($username)
-	{
-		$graph = new Graph();
-		$check['query'] = "MATCH n:User WHERE n.username = \"$username\" RETURN count(*);";
-		$api = $graph->neo4japi('cypher', 'JSONPOST', $check);
-
-		if ($api['data'][0][0] >= "1")
-		{
-			return false;
-		} else
-		{
-			return true;
-		}
-	}
-
-	/**
-	 * This method will check the graph database to ensure a email address is unique and valid
-	 * @param email
-	 * @return boolean (true if Available, false if Taken)
-	 */
-	public function validateEmailAddress($email)
-	{
-		$graph = new Graph();
-
-		$check['query'] = "MATCH n:User WHERE n.email = \"$email\" RETURN count(*);";
-		$api = $graph->neo4japi('cypher', 'JSONPOST', $check);
-
-		if ($api['data'][0][0] >= "1")
-		{
-			return false;
-		} else
-		{
-			return true;
-		}
-	}
-
-	/**
-	 * This method will check that the entered email address is valid.
-	 * @param email
-	 * @return boolean (true if Valid, false if Not)
-	 */
-	public function validateEmailFormatting($email)
-	{
-		if (!filter_var($email, FILTER_VALIDATE_EMAIL))
-		{
-			return false;
-		} else
-		{
-			return true;
-		}
-	}
-
-	/**
-	 * This method will check that the entered username is valid
-	 * @param username
-	 * @return boolean
-	 */
-	public function validateUsernameFormatting($username)
-	{
-		//Rules
-		if (strlen($username) < "4")
-		{
-			return false;
-			break;
-		} elseif (is_numeric($username))
-		{
-			return false;
-			break;
-		} elseif ($username === "admin" || $username === "invigle" || $username ===
-		"staff")
-		{
-			return false;
-			break;
-		} else
-		{
-			return true;
-			break;
-		}
-	}
-=======
-    private $_url;
+    	private $_url;
     
     /**
      * This method will check the graph database to ensure a username is unique.
@@ -201,7 +113,6 @@ class User
             break;
         }
     }
->>>>>>> origin/master
 
 	/**
 	 * This method takes as input an array with all the information of a user and adds this user to the GD as a 'user node'.
@@ -211,7 +122,6 @@ class User
 	 * 
 	 * @ReturnType boolean
 	 */
-<<<<<<< HEAD
 	public function addUser($aUserArray)
 	{
 		if (!$this->validateUsernameFormatting($aUserArray['username']))
@@ -308,11 +218,6 @@ class User
 		} else
 		{
 			return false;
-=======
-	public function addUser($aUserArray) {        
-        if (!$this->validateUsernameFormatting($aUserArray['username'])){
-            return 'username-invalid';
->>>>>>> origin/master
 		}
 
 
@@ -472,18 +377,6 @@ class User
 		$succ = $graph->addConnection($uID, $eID, $connectionType);
 		return $succ;
 	}
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
 
 	/**
 	 * This method takes as input the ID of a user and deletes the node that represent this user from the GD.
