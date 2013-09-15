750b4a3d5a93ad8e398b3e1f6f68c632215e89be
Rid <grant@seednet.eu>
2013-09-15 02:12:48 +0200

    merged with master

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-print 'Password is <b>';
-print strlen($_GET['password']);
-print ' </b>characters long<hr>';
-
-print 'HASHED PW: ';
-print hash('sha256', "kjwbgGRWwglrnwvbw242gf25g35thgwrWFEWDG25t2".$_GET['password']);
+require_once("rdb/rdb.php");
+echo r\systemInfo();
 ?>
